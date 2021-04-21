430787df49d425acd3b60e5d42885502583c0545
Fabiano Marcos <fabiano.marcos@seidor.com.br>
2021-04-21 19:37:38 -0300

    feature: dynamic data

--- lib/home/widgets/quiz_card/quiz_card_widget.dart
+++ lib/home/widgets/quiz_card/quiz_card_widget.dart
@@ -5,7 +5,16 @@ import 'package:DevQuiz/shared/widgets/progress_indicator/progress_indicator_wid
 import 'package:flutter/material.dart';
 
 class QuizCardWidget extends StatelessWidget {
-	const QuizCardWidget({Key? key}): super(key: key);
+	final String title;
+	final String completed;
+	final double percent;
+
+	const QuizCardWidget({
+		Key? key,
+		required this.title,
+		required this.completed,
+		required this.percent,
+	}): super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -22,7 +31,7 @@ class QuizCardWidget extends StatelessWidget {
 				Container(height: 40, width: 40, child: Image.asset(AppImages.blocks),),
 				SizedBox(height: 20),
 				Text(
-					"Gerenciamento de Estado",
+					title,
 					style: AppTextStyles.heading15,
 				),
 				SizedBox(height: 20),
@@ -30,14 +39,14 @@ class QuizCardWidget extends StatelessWidget {
 					Expanded(
 						flex: 1,
 						child: Text(
-							"3/10",
+							completed,
 							style: AppTextStyles.body11,
 						),
 					),
 					Expanded(
 						flex: 4,
 						child: ProgressIndicatorWidget(
-							value: 0.3,
+							value: percent,
 						)
 					)
 				],)
