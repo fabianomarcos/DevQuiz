69e71a5fe19154125bf49ab4fb2153a849d7fdd4
Fabiano Marcos <fabiano.marcos@seidor.com.br>
2021-04-24 15:41:29 -0300

    refactor: fixed identation and treatment action buttons

--- lib/challenge/challenge_page.dart
+++ lib/challenge/challenge_page.dart
@@ -2,13 +2,15 @@ import 'package:DevQuiz/challenge/challenge_controller.dart';
 import 'package:DevQuiz/challenge/widgets/next_button_widget.dart';
 import 'package:DevQuiz/challenge/widgets/question_indicator_widget.dart';
 import 'package:DevQuiz/challenge/widgets/quiz_widget.dart';
+import 'package:DevQuiz/result/result_page.dart';
 import 'package:DevQuiz/shared/models/question_model.dart';
 import 'package:flutter/material.dart';
 
 class ChallengePage extends StatefulWidget {
   final List<QuestionModel> questions;
+  final String title;
 
-  ChallengePage({Key? key, required this.questions}) : super(key: key);
+  ChallengePage({Key? key, required this.questions, required this.title}) : super(key: key);
 
   @override
   _ChallengePageState createState() => _ChallengePageState();
@@ -21,39 +23,43 @@ class _ChallengePageState extends State<ChallengePage> {
 	@override
 	void initState() {
 		pageController.addListener(() {
-		controller.currentPage = pageController.page!.toInt() + 1;
+			controller.currentPage = pageController.page!.toInt() + 1;
 		});
 		super.initState();
 	}
 
-	void nextPage() {
-		pageController.nextPage(
-			duration: Duration(milliseconds: 500),
-			curve: Curves.linear,
-		);
+	void nextPage(value) {
+		value == widget.questions.length
+			? Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) =>
+				ResultPage(title: widget.title, length: widget.questions.length, result: controller.counterRight,)))
+			: pageController.nextPage(duration: Duration(milliseconds: 500), curve: Curves.linear,);
+	}
+
+	void onSelected(bool value) {
+		if (value) controller.counterRight++;
 	}
 
 	@override
 	Widget build(BuildContext context) {
 		return Scaffold(
 			appBar: PreferredSize(
-			preferredSize: Size.fromHeight(86),
-			child: SafeArea(
-				top: true,
-				child: Column(
-				crossAxisAlignment: CrossAxisAlignment.start,
-				children: [
-					BackButton(),
-					ValueListenableBuilder<int>(
-					valueListenable: controller.currentPageNotifier,
-					builder: (context, value, _) => QuestionIndicatorWidget(
-						currentPage: value,
-						length: widget.questions.length,
+				preferredSize: Size.fromHeight(86),
+				child: SafeArea(
+					top: true,
+					child: Column(
+						crossAxisAlignment: CrossAxisAlignment.start,
+						children: [
+							BackButton(),
+							ValueListenableBuilder<int>(
+								valueListenable: controller.currentPageNotifier,
+								builder: (context, value, _) => QuestionIndicatorWidget(
+									currentPage: value,
+									length: widget.questions.length,
+								),
+							),
+						],
 					),
-					),
-				],
 				),
-			),
 			),
 			body: PageView(
 				physics: NeverScrollableScrollPhysics(),
@@ -62,32 +68,33 @@ class _ChallengePageState extends State<ChallengePage> {
 					widget.questions
 						.map((quiz) => QuizWidget(
 							question: quiz,
-							onChange: (){},
+							onSelected: onSelected,
 						)).toList(),
 			),
 			bottomNavigationBar: SafeArea(
 				child: Padding(
 					padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
 					child: ValueListenableBuilder<int>(
-							valueListenable: controller.currentPageNotifier,
-							builder: (context, value, _) => Row(
-								mainAxisAlignment: MainAxisAlignment.spaceAround,
-								children: [
-									if (value < widget.questions.length)
-										Expanded(
-											child: NextButtonWidget.white(
-											label: "Pular",
-											onTap: nextPage,
-										)),
-										SizedBox(width: 7),
+						valueListenable: controller.currentPageNotifier,
+						builder: (context, value, _) => Row(
+							mainAxisAlignment: MainAxisAlignment.spaceAround,
+							children: [
+								if (value < widget.questions.length)
 									Expanded(
-										child: NextButtonWidget.green(
-											label: value == widget.questions.length ? "Finalizar" : "Prosseguir",
-											onTap: value == widget.questions.length ? () {Navigator.pop(context);} : nextPage,
+										child: NextButtonWidget.white(
+											label: "Pular",
+											onTap: () => nextPage(value),
 										)
 									),
-								],
-							),
+									SizedBox(width: 7),
+								Expanded(
+									child: NextButtonWidget.green(
+										label: value == widget.questions.length ? "Finalizar" : "Prosseguir",
+										onTap: () => nextPage(value),
+									)
+								),
+							],
+						),
 					),
 				)
 			)
